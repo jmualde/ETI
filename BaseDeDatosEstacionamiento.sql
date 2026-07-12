539e22fe75d8d1c2f9cd3167b3b33521d089d736
facucenturion1 <facuthefredo1000@gmail.com>
2026-07-12 19:07:25 -0300

    actualizado

--- BaseDeDatosEstacionamiento.sql
+++ BaseDeDatosEstacionamiento.sql
@@ -3,7 +3,7 @@ USE estacionamiento;
 
 CREATE TABLE usuarios(
     id  INT PRIMARY KEY AUTO_INCREMENT,
-    nombreUsuario VARCHAR(50) NOT NULL,
+    nombreUsuario VARCHAR(50) NOT NULL UNIQUE,
     nombre VARCHAR(50) NOT NULL ,
     apellido VARCHAR(50) NOT NULL,
     email VARCHAR(200) NOT NULL UNIQUE,
