449a08b3dc3007a09f9a037401c0d679637d6ffa
Serrra10 <larecon741@gmail.com>
2026-08-31 08:15:55 -0300

    Update login.php

--- login.php
+++ login.php
@@ -12,7 +12,7 @@ $resultado = $conexion->query($sql);
     <title>Iniciar sesión</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
+    <link rel="stylesheet" type="text/css" href="disenio.css"> 
     <style>
     
 
